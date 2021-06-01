fe6040ed1f766d0c552c83a77d20277b131b392a
duke1014 <duke.norsworthy@gmail.com>
2021-06-01 16:12:20 -0400

    updated cell functionality

--- index.js
+++ index.js
@@ -6,30 +6,38 @@ document.addEventListener("DOMContentLoaded", () => {
 const randomDogApi = "https://dog.ceo/api/breeds/image/random";
 const dogImage = document.getElementById('dog')
 
+const turns = document.getElementById("turn");
+const grid = document.getElementById("grid");
+
 function startGame() {
-    const grid = document.getElementById("grid");
+    turnNumber = 1
     grid.innerHTML = "";
+    turns.innerHTML = "";
+    turns.innerHTML = `Blue's Turn! Turn ${turnNumber}`;
 
     for (let x = 0; x < 3; x++) {
         const row = grid.insertRow(x);
         for (let y = 0; y < 3; y++) {
             const cell = row.insertCell(y);
+            // cell.setAttribute("id", `${x}` + `${y}`)
             cell.onclick = function() {clickCell(this);}
-            cell.onmousedown = function(event) {
-                if (event.which == 3) {
-                    rightClickCell(this);
-                }
-            }
         }
     }
 }
+
 function clickCell(cell) {
-
-    cell.className = "clicked";
-
-}
-function rightClickCell(cell) {
-    cell.className = "right-clicked"
+    // console.log(cell)
+    let oddCounter = turnNumber % 2;
+    if (oddCounter == 0) {
+        cell.className = "clickedRed"; 
+        turnNumber = turnNumber + 1;
+        turns.innerHTML = `Blue's Turn! Turn ${turnNumber}`;
+    } else if (oddCounter == 1) {
+        cell.className = "clickedBlue";
+        turnNumber = turnNumber + 1;
+        turns.innerHTML = `Red's Turn! Turn ${turnNumber}`;
+    }
+    // return checkGameCompletion()
 }
 
 function appendDog () {
